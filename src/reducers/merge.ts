f0f1d5bd4805733c45aee702a8144ae974d124bf
Toofifty <alex@matho.me>
2020-06-16 19:22:08 +1000

    weird merge action thing im trying

---
+++ src/reducers/merge.ts
@@ -0,0 +1,35 @@
+export const _ = null as unknown;
+
+type Action<TType extends string, TPayload> = {
+    type: TType;
+    payload: TPayload;
+};
+
+type ActionObject<TState, TType extends string, TPayload> = {
+    type: TType;
+    action: (state: TState, payload: TPayload, dispatch: any) => TState;
+};
+
+export const action = <TState, TType extends string, TPayload>(
+    stateType: TState,
+    payloadType: TPayload,
+    type: TType,
+    action: (state: TState, payload: TPayload, dispatch: any) => TState
+) => ({
+    type,
+    action,
+});
+
+export type GetAction<TFrom extends ActionObject<any, any, any>> = Action<
+    TFrom['type'],
+    Parameters<TFrom['action']>[1]
+>;
+
+export const merge = <TState>(
+    defaultState?: TState,
+    ...actions: ActionObject<TState, string, any>[]
+) => (state = defaultState, action: any) => {
+    if (!state) return state;
+    const target = actions.find(({ type }) => type === action.type);
+    if (target) return target.action(state, action.payload, action.dispatch);
+};
--- src/reducers/shared.ts
+++ src/reducers/shared.ts
@@ -1,130 +1,105 @@
-import { RootState, WithDispatch, DispatchFn } from 'store';
-import { RootActionType } from 'reducers';
+import { RootState, DispatchFn } from 'store';
+import { _, action, merge, GetAction } from './merge';
 
 // put reducers here if:
 // - they require reading root state (or state from another slice)
 // - they dispatch actions into two or more slices
 
-type Reducer<T extends { payload: any }> = (
-    state: RootState,
-    payload: T['payload'],
-    dispatch: DispatchFn
-) => RootState;
+const setCellValue = action(
+    _ as RootState,
+    _ as { index: number; value?: number },
+    'shared/set-cell-value',
+    (state, { index, value }, dispatch: DispatchFn) => {
+        // TODO: switch where to place the digit based on sudoku mode
+        dispatch({ type: 'puzzle/set-given', payload: { index, value } });
 
-type SetCellValue = {
-    type: 'shared/set-cell-value';
-    payload: { index: number; value?: number };
-};
+        // TODO: check if solving enabled
+        const prevValue = state.sudoku.board[index].value;
+        // solve from scratch if value changed (or deleted)
+        const fromScratch = !!prevValue && value !== prevValue;
+        dispatch({ type: 'solver/trigger-solve', payload: fromScratch });
 
-const setCellValue: Reducer<SetCellValue> = (
-    state,
-    { index, value },
-    dispatch
-) => {
-    // TODO: switch where to place the digit based on sudoku mode
-    dispatch({ type: 'puzzle/set-given', payload: { index, value } });
+        return state;
+    }
+);
 
-    // TODO: check if solving enabled
-    const prevValue = state.sudoku.board[index].value;
-    // solve from scratch if value changed (or deleted)
-    const fromScratch = !!prevValue && value !== prevValue;
-    dispatch({ type: 'solver/trigger-solve', payload: fromScratch });
+const setRestrictions = action(
+    _ as RootState,
+    _ as Partial<RootState['sudoku']['restrictions']>,
+    'shared/set-restrictions',
+    (state, restrictions, dispatch: DispatchFn) => {
+        dispatch({ type: 'puzzle/set-restrictions', payload: restrictions });
 
-    return state;
-};
+        // enable/disable relevant algorithms automatically
+        // TODO: check if solving enabled
+        const algorithms = Object.entries(restrictions).reduce(
+            (acc, [restriction, value]) => {
+                if (restriction in state.solver.algorithms) {
+                    return { acc, [restriction]: value };
+                }
+                return acc;
+            },
+            {} as any
+        );
+        dispatch({ type: 'solver/set-algorithms', payload: algorithms });
+        dispatch({ type: 'solver/trigger-solve', payload: true });
 
-type SetRestrictions = {
-    type: 'shared/set-restrictions';
-    payload: Partial<RootState['sudoku']['restrictions']>;
-};
+        return state;
+    }
+);
 
-const setRestrictions: Reducer<SetRestrictions> = (
-    state,
-    restrictions,
-    dispatch
-) => {
-    dispatch({ type: 'puzzle/set-restrictions', payload: restrictions });
+const setAlgorithms = action(
+    _ as RootState,
+    _ as Partial<RootState['solver']['algorithms']>,
+    'shared/set-algorithms',
+    (state, algorithms, dispatch: DispatchFn) => {
+        dispatch({ type: 'solver/set-algorithms', payload: algorithms });
 
-    // enable/disable relevant algorithms automatically
-    // TODO: check if solving enabled
-    const algorithms = Object.entries(restrictions).reduce(
-        (acc, [restriction, value]) => {
-            if (restriction in state.solver.algorithms) {
-                return { acc, [restriction]: value };
-            }
-            return acc;
-        },
-        {} as any
-    );
-    dispatch({ type: 'solver/set-algorithms', payload: algorithms });
-    dispatch({ type: 'solver/trigger-solve', payload: true });
+        // enable relevant restrictions automatically
+        const restrictions = Object.entries(algorithms).reduce(
+            (acc, [algorithm, value]) => {
+                if (value && algorithm in state.sudoku.restrictions) {
+                    return { acc, [algorithm]: true };
+                }
+                return acc;
+            },
+            {} as any
+        );
+        dispatch({ type: 'puzzle/set-restrictions', payload: restrictions });
+        dispatch({ type: 'solver/trigger-solve', payload: true });
 
-    return state;
-};
+        return state;
+    }
+);
 
-type SetAlgorithms = {
-    type: 'shared/set-algorithms';
-    payload: Partial<RootState['solver']['algorithms']>;
-};
+const reset = action(
+    _ as RootState,
+    _ as undefined,
+    'shared/reset',
+    (state, _, dispatch: DispatchFn) => {
+        // TODO: if in player mode, don't reset the puzzle
+        dispatch({ type: 'puzzle/reset', payload: undefined });
+        dispatch(() => {
+            window.location.hash = '';
+        });
 
-const setAlgorithms: Reducer<SetAlgorithms> = (state, algorithms, dispatch) => {
-    dispatch({ type: 'solver/set-algorithms', payload: algorithms });
+        // reset solution
+        dispatch({ type: 'solver/reset-solution', payload: undefined });
 
-    // enable relevant restrictions automatically
-    const restrictions = Object.entries(algorithms).reduce(
-        (acc, [algorithm, value]) => {
-            if (value && algorithm in state.sudoku.restrictions) {
-                return { acc, [algorithm]: true };
-            }
-            return acc;
-        },
-        {} as any
-    );
-    dispatch({ type: 'puzzle/set-restrictions', payload: restrictions });
-    dispatch({ type: 'solver/trigger-solve', payload: true });
-
-    return state;
-};
-
-type Reset = {
-    type: 'shared/reset';
-    payload: undefined;
-};
-
-const reset: Reducer<Reset> = (state, _, dispatch) => {
-    // TODO: if in player mode, don't reset the puzzle
-    dispatch({ type: 'puzzle/reset', payload: undefined });
-    dispatch(() => {
-        window.location.hash = '';
-    });
-
-    // reset solution
-    dispatch({ type: 'solver/reset-solution', payload: undefined });
-
-    return state;
-};
+        return state;
+    }
+);
 
 export type SharedAction =
-    | SetCellValue
-    | SetRestrictions
-    | SetAlgorithms
-    | Reset;
+    | GetAction<typeof setCellValue>
+    | GetAction<typeof setRestrictions>
+    | GetAction<typeof setAlgorithms>
+    | GetAction<typeof reset>;
 
-export default (
-    state: RootState | undefined,
-    action: RootActionType & WithDispatch
-) => {
-    if (!state) return state;
-    switch (action.type) {
-        case 'shared/set-cell-value':
-            return setCellValue(state, action.payload, action.dispatch);
-        case 'shared/set-restrictions':
-            return setRestrictions(state, action.payload, action.dispatch);
-        case 'shared/set-algorithms':
-            return setAlgorithms(state, action.payload, action.dispatch);
-        case 'shared/reset':
-            return reset(state, action.payload, action.dispatch);
-        default:
-            return state;
-    }
-};
+export default merge<RootState>(
+    undefined,
+    setCellValue,
+    setRestrictions,
+    setAlgorithms,
+    reset
+);
